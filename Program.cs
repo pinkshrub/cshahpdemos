79fe8f9ada98edc2f5f0aec0a8a5d23358272b99
Alan Weber <pinkshrub@gmail.com>
2017-08-01 16:44:39 -0700

    the first stuff yo

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace doc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Card jon = new Card("jon", "black", 1);
+            System.Console.WriteLine(jon.val);
+            Deck bob = new Deck().reset();
+            System.Console.WriteLine(bob.cards.Count);
+            Player bill = new Player("Rickardendondolo");
+            bill.draw(bob,5);
+            System.Console.WriteLine(bill.hand.Count);
+            bill.discard(4);
+            System.Console.WriteLine(bill.hand.Count);
+        }
+    }
+}
